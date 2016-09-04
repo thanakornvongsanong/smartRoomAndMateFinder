f10cea90dd613cf78270750785aae6c1ddbd72c5
thanakornvongsanong <thanakorn.vongsanong@gmail.com>
2016-09-04 15:23:43 +0700

    Header 100%

---
+++ View/headerLogined.php
@@ -0,0 +1,49 @@
+<!-- Navigation -->
+        <nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top" style="background-color:black">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header page-scroll">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                    </button>
+                    <a class="navbar-brand page-scroll" href="#page-top">Smart Room and Mate Finder</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="hidden">
+                            <a href="#page-top"></a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="index.html">Home</a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" >Find <span class="caret"></span></a>
+                                <ul class="dropdown-menu " style="background-color:black">
+                                        <li><a href="findroommateandcompare.php" >Find Roommate</a></li>
+                                        <li role="separator" class="divider"></li>
+                                        <li><a href="findroomandcompare.php">Find Room</a></li>
+                                </ul>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="request.html">Request</a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" >User Name <span class="caret"></span></a>
+                                <ul class="dropdown-menu " style="background-color:black">
+                                        <li><a href="profile.php" >Profile</a></li>
+                                        <li role="separator" class="divider"></li>
+                                        <li><a href="index.php">Logout</a></li>
+                                </ul>
+                        </li>
+                        <li>
+                            <a ><i class="fa fa-bell" style="color:#fecd1b"><span class="badge badge-notify;position:absolute;top:0px;" style="color:#fecd1b">99</span></i></a>
+                        </li>
+
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
--- View/index.php
+++ View/index.php
@@ -1,3 +1,7 @@
+<?php 
+    session_start();
+    
+ ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -26,6 +30,8 @@
     <link href="css/myStyle.css" rel="stylesheet">
     <script src="css/myJS.js"></script>
 
+    <!--Font Awesome-->
+     <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -38,41 +44,12 @@
 
 <body id="page-top" class="index">
 
-    <!-- Navigation -->
-    <nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
-                </button>
-                <a class="navbar-brand page-scroll" href="#page-top">Smart Room and Mate Finder</a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="hidden">
-                        <a href="#page-top"></a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#services">Services</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#about">Guide</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#portfolio">Login with Facebook</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#contact">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container-fluid -->
-    </nav>
+    <?php 
+                include "headerLogined.php";
+            }else{
+                include "headerNotLogin.php";
+            }
+    ?>
 
     <!-- Header -->
     <header>
