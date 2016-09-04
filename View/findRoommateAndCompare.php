3753809e042ccd2a75db29ca6b111d0f9021b1ca
thanakornvongsanong <thanakorn.vongsanong@gmail.com>
2016-09-04 13:48:48 +0700

    Find roommate and Compare 95%
    *Bug when change resolution

--- View/findRoommateAndCompare.php
+++ View/findRoommateAndCompare.php
@@ -27,10 +27,10 @@
 				<span></span>
 				<h1>Smart Room and Mate Finder</h1>
 				<nav>
-					<a href="http://tympanus.net/Blueprints/GridGallery/" class="bp-icon bp-icon-prev" data-info="previous Blueprint"><span>Previous Blueprint</span></a>
+					<a href="#" class="bp-icon bp-icon-prev" data-info="Home"><span>Home</span></a>
 					<!--a href="" class="bp-icon bp-icon-next" data-info="next Blueprint"><span>Next Blueprint</span></a-->
-					<a href="http://tympanus.net/codrops/?p=24096" class="bp-icon bp-icon-drop" data-info="back to the Codrops article"><span>back to the Codrops article</span></a>
-					<a href="http://tympanus.net/codrops/category/blueprints/" class="bp-icon bp-icon-archive" data-info="Blueprints archive"><span>Go to the archive</span></a>
+					<!--a href="#" class="bp-icon bp-icon-drop" data-info="back to the Codrops article"><span>back to the Codrops article</span></a-->
+					<a href="#" class="bp-icon bp-icon-archive" data-info="Profile"><span>Profile</span></a>
 				</nav>
 			</header>
 			<!-- Product grid -->
@@ -40,11 +40,17 @@
 					<div class="product__info">
 						<img class="product__image" src="img/userCompare.png" alt="Product 1" />
 						<h3 class="product__title">Chryseia</h3>
-						<span class="product__any extra highlight">2011</span>
-						<span class="product__any extra highlight">Douro</span>
-						<span class="product__any extra highlight">Touriga Nacional</span>
-						<span class="product__any extra highlight">13%</span>
-						<span class="product__any highlight">$55.90</span>
+						<span class="product__any extra highlight">Clean: </span>
+						<span class="product__any extra highlight">Party:</span>
+						<span class="product__any extra highlight">Snore: </span>
+						<span class="product__any extra highlight">Smoke: </span>
+						<span class="product__any extra highlight">Status: </span>
+						<span class="product__any extra highlight">Rental period: </span>
+						<span class="product__any extra highlight">Rental cost: </span>
+						<span class="product__any extra highlight">Room already has: </span>
+						<span class="product__any extra highlight">Pets: </span>
+						<span class="product__any extra highlight">Have children: </span>
+						<span class="product__any  highlight">Approx. move in date: </span>
 						<button class="action action--button action--buy"><i class="fa fa-paper-plane"></i><span class="action__text">Send Request</span></button>
 					</div>
 					<label class="action action--compare-add"><input class="check-hidden" type="checkbox" /><i class="fa fa-plus"></i><i class="fa fa-check"></i><span class="action__text action__text--invisible">Add to compare</span></label>
@@ -53,83 +59,188 @@
 					<div class="product__info">
 						<img class="product__image" src="img/userCompare.png" alt="Product 1" />
 						<h3 class="product__title">Chryseia</h3>
-						<span class="product__any extra highlight">2011</span>
-						<span class="product__any extra highlight">Douro</span>
-						<span class="product__any extra highlight">Touriga Nacional</span>
-						<span class="product__any extra highlight">13%</span>
-						<span class="product__any highlight">$55.90</span>
+						<span class="product__any extra highlight">Clean: </span>
+						<span class="product__any extra highlight">Party:</span>
+						<span class="product__any extra highlight">Snore: </span>
+						<span class="product__any extra highlight">Smoke: </span>
+						<span class="product__any extra highlight">Status: </span>
+						<span class="product__any extra highlight">Rental period: </span>
+						<span class="product__any extra highlight">Rental cost: </span>
+						<span class="product__any extra highlight">Room already has: </span>
+						<span class="product__any extra highlight">Pets: </span>
+						<span class="product__any extra highlight">Have children: </span>
+						<span class="product__any  highlight">Approx. move in date: </span>
 						<button class="action action--button action--buy"><i class="fa fa-paper-plane"></i><span class="action__text">Send Request</span></button>
 					</div>
 					<label class="action action--compare-add"><input class="check-hidden" type="checkbox" /><i class="fa fa-plus"></i><i class="fa fa-check"></i><span class="action__text action__text--invisible">Add to compare</span></label>
-				</div><div class="product">
+				</div>
+				<div class="product">
 					<div class="product__info">
 						<img class="product__image" src="img/userCompare.png" alt="Product 1" />
 						<h3 class="product__title">Chryseia</h3>
-						<span class="product__any extra highlight">2011</span>
-						<span class="product__any extra highlight">Douro</span>
-						<span class="product__any extra highlight">Touriga Nacional</span>
-						<span class="product__any extra highlight">13%</span>
-						<span class="product__any highlight">$55.90</span>
+						<span class="product__any extra highlight">Clean: </span>
+						<span class="product__any extra highlight">Party:</span>
+						<span class="product__any extra highlight">Snore: </span>
+						<span class="product__any extra highlight">Smoke: </span>
+						<span class="product__any extra highlight">Status: </span>
+						<span class="product__any extra highlight">Rental period: </span>
+						<span class="product__any extra highlight">Rental cost: </span>
+						<span class="product__any extra highlight">Room already has: </span>
+						<span class="product__any extra highlight">Pets: </span>
+						<span class="product__any extra highlight">Have children: </span>
+						<span class="product__any  highlight">Approx. move in date: </span>
 						<button class="action action--button action--buy"><i class="fa fa-paper-plane"></i><span class="action__text">Send Request</span></button>
 					</div>
 					<label class="action action--compare-add"><input class="check-hidden" type="checkbox" /><i class="fa fa-plus"></i><i class="fa fa-check"></i><span class="action__text action__text--invisible">Add to compare</span></label>
-				</div><div class="product">
+				</div>
+				<div class="product">
 					<div class="product__info">
 						<img class="product__image" src="img/userCompare.png" alt="Product 1" />
 						<h3 class="product__title">Chryseia</h3>
-						<span class="product__any extra highlight">2011</span>
-						<span class="product__any extra highlight">Douro</span>
-						<span class="product__any extra highlight">Touriga Nacional</span>
-						<span class="product__any extra highlight">13%</span>
-						<span class="product__any highlight">$55.90</span>
+						<span class="product__any extra highlight">Clean: </span>
+						<span class="product__any extra highlight">Party:</span>
+						<span class="product__any extra highlight">Snore: </span>
+						<span class="product__any extra highlight">Smoke: </span>
+						<span class="product__any extra highlight">Status: </span>
+						<span class="product__any extra highlight">Rental period: </span>
+						<span class="product__any extra highlight">Rental cost: </span>
+						<span class="product__any extra highlight">Room already has: </span>
+						<span class="product__any extra highlight">Pets: </span>
+						<span class="product__any extra highlight">Have children: </span>
+						<span class="product__any  highlight">Approx. move in date: </span>
 						<button class="action action--button action--buy"><i class="fa fa-paper-plane"></i><span class="action__text">Send Request</span></button>
 					</div>
 					<label class="action action--compare-add"><input class="check-hidden" type="checkbox" /><i class="fa fa-plus"></i><i class="fa fa-check"></i><span class="action__text action__text--invisible">Add to compare</span></label>
-				</div><div class="product">
+				</div>
+				<div class="product">
 					<div class="product__info">
 						<img class="product__image" src="img/userCompare.png" alt="Product 1" />
 						<h3 class="product__title">Chryseia</h3>
-						<span class="product__any extra highlight">2011</span>
-						<span class="product__any extra highlight">Douro</span>
-						<span class="product__any extra highlight">Touriga Nacional</span>
-						<span class="product__any extra highlight">13%</span>
-						<span class="product__any highlight">$55.90</span>
+						<span class="product__any extra highlight">Clean: </span>
+						<span class="product__any extra highlight">Party:</span>
+						<span class="product__any extra highlight">Snore: </span>
+						<span class="product__any extra highlight">Smoke: </span>
+						<span class="product__any extra highlight">Status: </span>
+						<span class="product__any extra highlight">Rental period: </span>
+						<span class="product__any extra highlight">Rental cost: </span>
+						<span class="product__any extra highlight">Room already has: </span>
+						<span class="product__any extra highlight">Pets: </span>
+						<span class="product__any extra highlight">Have children: </span>
+						<span class="product__any  highlight">Approx. move in date: </span>
 						<button class="action action--button action--buy"><i class="fa fa-paper-plane"></i><span class="action__text">Send Request</span></button>
 					</div>
 					<label class="action action--compare-add"><input class="check-hidden" type="checkbox" /><i class="fa fa-plus"></i><i class="fa fa-check"></i><span class="action__text action__text--invisible">Add to compare</span></label>
-				</div><div class="product">
+				</div>
+				<div class="product">
 					<div class="product__info">
 						<img class="product__image" src="img/userCompare.png" alt="Product 1" />
 						<h3 class="product__title">Chryseia</h3>
-						<span class="product__any extra highlight">2011</span>
-						<span class="product__any extra highlight">Douro</span>
-						<span class="product__any extra highlight">Touriga Nacional</span>
-						<span class="product__any extra highlight">13%</span>
-						<span class="product__any highlight">$55.90</span>
+						<span class="product__any extra highlight">Clean: </span>
+						<span class="product__any extra highlight">Party:</span>
+						<span class="product__any extra highlight">Snore: </span>
+						<span class="product__any extra highlight">Smoke: </span>
+						<span class="product__any extra highlight">Status: </span>
+						<span class="product__any extra highlight">Rental period: </span>
+						<span class="product__any extra highlight">Rental cost: </span>
+						<span class="product__any extra highlight">Room already has: </span>
+						<span class="product__any extra highlight">Pets: </span>
+						<span class="product__any extra highlight">Have children: </span>
+						<span class="product__any  highlight">Approx. move in date: </span>
 						<button class="action action--button action--buy"><i class="fa fa-paper-plane"></i><span class="action__text">Send Request</span></button>
 					</div>
 					<label class="action action--compare-add"><input class="check-hidden" type="checkbox" /><i class="fa fa-plus"></i><i class="fa fa-check"></i><span class="action__text action__text--invisible">Add to compare</span></label>
-				</div><div class="product">
+				</div>
+				<div class="product">
 					<div class="product__info">
 						<img class="product__image" src="img/userCompare.png" alt="Product 1" />
 						<h3 class="product__title">Chryseia</h3>
-						<span class="product__any extra highlight">2011</span>
-						<span class="product__any extra highlight">Douro</span>
-						<span class="product__any extra highlight">Touriga Nacional</span>
-						<span class="product__any extra highlight">13%</span>
-						<span class="product__any highlight">$55.90</span>
+						<span class="product__any extra highlight">Clean: </span>
+						<span class="product__any extra highlight">Party:</span>
+						<span class="product__any extra highlight">Snore: </span>
+						<span class="product__any extra highlight">Smoke: </span>
+						<span class="product__any extra highlight">Status: </span>
+						<span class="product__any extra highlight">Rental period: </span>
+						<span class="product__any extra highlight">Rental cost: </span>
+						<span class="product__any extra highlight">Room already has: </span>
+						<span class="product__any extra highlight">Pets: </span>
+						<span class="product__any extra highlight">Have children: </span>
+						<span class="product__any  highlight">Approx. move in date: </span>
 						<button class="action action--button action--buy"><i class="fa fa-paper-plane"></i><span class="action__text">Send Request</span></button>
 					</div>
 					<label class="action action--compare-add"><input class="check-hidden" type="checkbox" /><i class="fa fa-plus"></i><i class="fa fa-check"></i><span class="action__text action__text--invisible">Add to compare</span></label>
-				</div><div class="product">
+				</div>
+				<div class="product">
 					<div class="product__info">
 						<img class="product__image" src="img/userCompare.png" alt="Product 1" />
 						<h3 class="product__title">Chryseia</h3>
-						<span class="product__any extra highlight">2011</span>
-						<span class="product__any extra highlight">Douro</span>
-						<span class="product__any extra highlight">Touriga Nacional</span>
-						<span class="product__any extra highlight">13%</span>
-						<span class="product__any highlight">$55.90</span>
+						<span class="product__any extra highlight">Clean: </span>
+						<span class="product__any extra highlight">Party:</span>
+						<span class="product__any extra highlight">Snore: </span>
+						<span class="product__any extra highlight">Smoke: </span>
+						<span class="product__any extra highlight">Status: </span>
+						<span class="product__any extra highlight">Rental period: </span>
+						<span class="product__any extra highlight">Rental cost: </span>
+						<span class="product__any extra highlight">Room already has: </span>
+						<span class="product__any extra highlight">Pets: </span>
+						<span class="product__any extra highlight">Have children: </span>
+						<span class="product__any  highlight">Approx. move in date: </span>
+						<button class="action action--button action--buy"><i class="fa fa-paper-plane"></i><span class="action__text">Send Request</span></button>
+					</div>
+					<label class="action action--compare-add"><input class="check-hidden" type="checkbox" /><i class="fa fa-plus"></i><i class="fa fa-check"></i><span class="action__text action__text--invisible">Add to compare</span></label>
+				</div>
+				<div class="product">
+					<div class="product__info">
+						<img class="product__image" src="img/userCompare.png" alt="Product 1" />
+						<h3 class="product__title">Chryseia</h3>
+						<span class="product__any extra highlight">Clean: </span>
+						<span class="product__any extra highlight">Party:</span>
+						<span class="product__any extra highlight">Snore: </span>
+						<span class="product__any extra highlight">Smoke: </span>
+						<span class="product__any extra highlight">Status: </span>
+						<span class="product__any extra highlight">Rental period: </span>
+						<span class="product__any extra highlight">Rental cost: </span>
+						<span class="product__any extra highlight">Room already has: </span>
+						<span class="product__any extra highlight">Pets: </span>
+						<span class="product__any extra highlight">Have children: </span>
+						<span class="product__any  highlight">Approx. move in date: </span>
+						<button class="action action--button action--buy"><i class="fa fa-paper-plane"></i><span class="action__text">Send Request</span></button>
+					</div>
+					<label class="action action--compare-add"><input class="check-hidden" type="checkbox" /><i class="fa fa-plus"></i><i class="fa fa-check"></i><span class="action__text action__text--invisible">Add to compare</span></label>
+				</div>
+				<div class="product">
+					<div class="product__info">
+						<img class="product__image" src="img/userCompare.png" alt="Product 1" />
+						<h3 class="product__title">Chryseia</h3>
+						<span class="product__any extra highlight">Clean: </span>
+						<span class="product__any extra highlight">Party:</span>
+						<span class="product__any extra highlight">Snore: </span>
+						<span class="product__any extra highlight">Smoke: </span>
+						<span class="product__any extra highlight">Status: </span>
+						<span class="product__any extra highlight">Rental period: </span>
+						<span class="product__any extra highlight">Rental cost: </span>
+						<span class="product__any extra highlight">Room already has: </span>
+						<span class="product__any extra highlight">Pets: </span>
+						<span class="product__any extra highlight">Have children: </span>
+						<span class="product__any  highlight">Approx. move in date: </span>
+						<button class="action action--button action--buy"><i class="fa fa-paper-plane"></i><span class="action__text">Send Request</span></button>
+					</div>
+					<label class="action action--compare-add"><input class="check-hidden" type="checkbox" /><i class="fa fa-plus"></i><i class="fa fa-check"></i><span class="action__text action__text--invisible">Add to compare</span></label>
+				</div>
+				<div class="product">
+					<div class="product__info">
+						<img class="product__image" src="img/userCompare.png" alt="Product 1" />
+						<h3 class="product__title">Chryseia</h3>
+						<span class="product__any extra highlight">Clean: </span>
+						<span class="product__any extra highlight">Party:</span>
+						<span class="product__any extra highlight">Snore: </span>
+						<span class="product__any extra highlight">Smoke: </span>
+						<span class="product__any extra highlight">Status: </span>
+						<span class="product__any extra highlight">Rental period: </span>
+						<span class="product__any extra highlight">Rental cost: </span>
+						<span class="product__any extra highlight">Room already has: </span>
+						<span class="product__any extra highlight">Pets: </span>
+						<span class="product__any extra highlight">Have children: </span>
+						<span class="product__any  highlight">Approx. move in date: </span>
 						<button class="action action--button action--buy"><i class="fa fa-paper-plane"></i><span class="action__text">Send Request</span></button>
 					</div>
 					<label class="action action--compare-add"><input class="check-hidden" type="checkbox" /><i class="fa fa-plus"></i><i class="fa fa-check"></i><span class="action__text action__text--invisible">Add to compare</span></label>
