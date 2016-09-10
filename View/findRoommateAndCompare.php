6a1973aceeada2b6f0e2bc121e7a37b61174c784
thanakornvongsanong <thanakorn.vongsanong@gmail.com>
2016-09-10 12:55:23 +0700

    k

--- View/findRoommateAndCompare.php
+++ View/findRoommateAndCompare.php
@@ -8,18 +8,19 @@
 		<title>Find Roommate</title>
 		<meta name="description" content="Blueprint: A basic responsive product grid layout with comparison functionality" />
 		<meta name="keywords" content="blueprint, template, html, css, javascript, grid, layout, effect, product comparison" />
-		<meta name="author" content="Codrops" />
-		<link rel="shortcut icon" href="favicon.ico">
 		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 		<link rel="stylesheet" type="text/css" href="css/demo.css" />
 		<link rel="stylesheet" type="text/css" href="css/component.css" />
 		<!-- Modernizr is used for flexbox fallback -->
 		<script src="js/modernizr.custom.js"></script>
 		
-		<!--Menu CSS -->
+		<Menu CSS >
 		<link rel="stylesheet" type="text/css" href="css/normalizeMenu.css" />
-		<link rel="stylesheet" type="text/css" href="css/demoMenu.css" />
+		<link rel="stylesheet" type="text/css" href="css/demoMenu.css"/>
 		<link rel="stylesheet" type="text/css" href="css/menu_topexpand.css" />
+
+		<!--JQuery-->
+		<script src="vendor/jquery/jquery.js"></script>
 
 		<!--Bootstrap-->
 		<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.css" />
@@ -382,17 +383,15 @@
 						
 						<button class="action action--button action--buy"><i class="fa fa-paper-plane"></i><span class="action__text">Send Request</span></button>
 					</div>
-					<label class="action action--compare-add"><input class="check-hidden" type="checkbox" /><i class="fa fa-plus"></i><i class="fa fa-check"></i><span class="action__text action__text--invisible">Add to compare</span></label>
+					<label class="action action--compare-add"><input class="check-hidden" type="checkbox" /><i class="fa fa-plus"></i><i class="fa fa-check" ></i><span class="action__text action__text--invisible">Add to compare</span></label>
 				</div>
 			</section>
 			</div>
 			</div>
-			
-			
 		</div><!-- /view -->
 		<!-- product compare wrapper -->
-		<section class="compare">
-			<button class="action action--close"><i class="fa fa-remove"></i><span class="action__text action__text--invisible">Close comparison overlay</span></button>
+		<section class="compare"><!--class="fa fa-remove"-->
+			<button class="action action--close"><i class="fa fa-remove" style="color:black"></i><span class="action__text action__text--invisible">Close comparison overlay</span></button>
 		</section>
 		<script src="js/classie.js"></script>
 		<script src="js/main.js"></script>
